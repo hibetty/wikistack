6778da59b633542a7f56e177db707fd7b07be780
Betty Lin <bettyhappylin@gmail.com>
2017-04-28 17:09:09 -0400

    start workshop, progress in Sequelize Essentials section

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
+var app = express();
+var models = require('./models');
+
+//serve static files
+app.use(express.static('public'));
+
+//logging middleware
+app.use(morgan('dev'));
+
+// nunjucks templating setup
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true});
+
+//sync databases
+models.db.sync({force: true})
+  .then(function(){
+    app.listen(8080, function(){
+       console.log('listening on port 8080');
+    });
+  })
+  .catch(console.error);
+
+app.get('/', function(req, res, next){
+  res.render('index');
+});
---
+++ models/index.js
@@ -0,0 +1,20 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+var Page =  db.define('page', {
+  title: { type: Sequelize.STRING, allowNull: false },
+  urlTitle: { type: Sequelize.STRING, allowNull: false },
+  content: { type: Sequelize.TEXT, allowNull: false },
+  status: Sequelize.ENUM('open', 'closed')
+});
+
+var User =  db.define('user', {
+  name: { type: Sequelize.STRING, allowNull: false, validate: {isAlpha: true} },
+  email: { type: Sequelize.STRING, allowNull: false, validate: {isEmail: true} }
+});
+
+module.exports = {
+  Page: Page,
+  User: User,
+  db: db
+};
